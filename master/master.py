cf15bdaceda43e1f37900deef138b6bf34398d6f
Alexandr <s.lebedev.2011@bk.ru>
2020-09-16 11:34:08 +0300

    add http-test-suite submodule

--- master/master.py
+++ master/master.py
@@ -1,18 +1,21 @@
-import asyncio
+from asyncio.streams import StreamReader, StreamWriter
 
-async def master_job(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
+
+async def master_job(reader: StreamReader, writer: StreamWriter):
+    request_line_encoded = await reader.readline()
+    request_line = request_line_encoded.decode()
+    request_line_words = request_line.split(' ')
+    if len(request_line_words) < 2:
+        # FIXME(Alex): Error handling
+        pass
+
+    method, path = request_line_words[:2]
+
     addr = writer.get_extra_info('peername')
-    print("Received message from {addr}".format(addr=addr))
+    print("{addr}:\t{method} {path}".format(addr=addr, method=method, path=path))
 
-    print("Send: {message}".format(message=message))
-
-    await asyncio.sleep(10)
-    writer.write(data)
+    writer.write(request_line_encoded)
     await writer.drain()
-
-    print("Close the client socket")
     writer.close()
 
     print("=========================================")
