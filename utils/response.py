1e678492482c5b2572addba59c3f452b8d9689e1
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2020-09-22 03:02:10 +0300

    doesnt work properly

--- utils/response.py
+++ utils/response.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from os.path import getsize
 from socket import socket
 from config import Config
+from aiofile import AIOFile
 
 STATUS_MESSAGES = {
     200: 'OK',
@@ -52,8 +53,8 @@ class Response:
         await loop.sock_sendall(client_socket, raw_bytes)
 
         if self._filepath is not None and self._method == 'GET':
-            with open(self._filepath, 'rb') as file:
-                part = file.read(Config.bytes_per_send)
+            async with AIOFile(self._filepath, 'rb') as file:
+                part = await file.read(Config.bytes_per_send)
                 while len(part) > 0:
                     await loop.sock_sendall(client_socket, part)
-                    part = file.read(Config.bytes_per_send)
+                    part = await file.read(Config.bytes_per_send)
